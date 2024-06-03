58d629b8ec44d48867ed190dab728b19281f1713
Guilherme Avelino <gavelino@gmail.com>
2024-06-03 15:46:16 -0300

    new change app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ES2 TEST
         </p>
         <a
           className="App-link"
